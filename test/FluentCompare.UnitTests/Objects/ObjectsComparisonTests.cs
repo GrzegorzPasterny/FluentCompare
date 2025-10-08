2d22c22f994aba62cff61fe09870c0033b1f806e
Grzegorz Pasterny <grzegorz.pasterny95@gmail.com>
2025-10-08 07:09:25 +0200

    Add more test cases for objects

--- test/FluentCompare.UnitTests/Objects/ObjectsComparisonTests.cs
+++ test/FluentCompare.UnitTests/Objects/ObjectsComparisonTests.cs
@@ -35,7 +35,7 @@ namespace FluentCompare.UnitTests.Objects
         }
 
         [Fact]
-        public void Compare_TwoNonEquivalentAnonymousTypes_WithDefaultConfiguration_ReturnsNotEqualResult()
+        public void Compare_TwoNonEquivalentAnonymousTypes_WithDefaultConfiguration_ReturnsNotMatchingResult()
         {
             // Arrange
             var obj1 = new { Name = "Test", Value = 123 };
@@ -47,6 +47,61 @@ namespace FluentCompare.UnitTests.Objects
 
             // Assert
             result.AllMatched.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void Compare_TwoEquivalentAnonymousTypeArrays_WithDefaultConfiguration_ReturnsAllMatchingResult()
+        {
+            // Arrange
+            var obj1 = new { Name = "Test", Value = 123 };
+            var obj2 = new { Name = "Test", Value = 456 };
+            var objArr1 = new[] { obj1, obj2 };
+
+            var obj3 = new { Name = "Test", Value = 123 };
+            var obj4 = new { Name = "Test", Value = 456 };
+            var objArr2 = new[] { obj3, obj4 };
+
+            // Act
+            var result = new ComparisonBuilder()
+                .Compare(obj1, obj2);
+
+            // Assert
+            result.AllMatched.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void Compare_TwoSameAnonymousTypeArrays_WithDefaultConfiguration_ReturnsAllMatchingResult()
+        {
+            // Arrange
+            var obj1 = new { Name = "Test", Value = 123 };
+            var obj2 = new { Name = "Test", Value = 456 };
+            var objArr1 = new[] { obj1, obj2 };
+            var objArr2 = new[] { obj1, obj2 };
+
+            // Act
+            var result = new ComparisonBuilder()
+                .Compare(obj1, obj2);
+
+            // Assert
+            result.AllMatched.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Compare_TwoSameAnonymousTypeArrays_WithReferenceComparison_ReturnsAllMatchingResult()
+        {
+            // Arrange
+            var obj1 = new { Name = "Test", Value = 123 };
+            var obj2 = new { Name = "Test", Value = 456 };
+            var objArr1 = new[] { obj1, obj2 };
+            var objArr2 = new[] { obj1, obj2 };
+
+            // Act
+            var result = new ComparisonBuilder()
+                .UseReferenceEquality()
+                .Compare(obj1, obj2);
+
+            // Assert
+            result.AllMatched.ShouldBeTrue();
         }
     }
 }
