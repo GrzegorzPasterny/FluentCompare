62469acf9dd104ce8946dd34d7cd4a94029b51b2
Grzegorz Pasterny <grzegorz.pasterny95@gmail.com>
2025-10-14 05:54:07 +0200

    Add configuration parameters and summaries

--- src/FluentCompare/Configuration/ComparisonConfiguration.cs
+++ src/FluentCompare/Configuration/ComparisonConfiguration.cs
@@ -1,12 +1,36 @@
-
+/// <summary>
+/// FluentCompare configuration object
+/// </summary>
 public class ComparisonConfiguration
 {
+    /// <summary>
+    /// The type of comparison to perform. Defaults to EqualTo.
+    /// </summary>
     public ComparisonType ComparisonType { get; set; }
         = ComparisonType.EqualTo;
 
+    /// <summary>
+    /// Defines how complex types (types that contain other complex or primitive types) are compared.
+    /// </summary>
     public ComplexTypesComparisonMode ComplexTypesComparisonMode { get; set; }
         = ComplexTypesComparisonMode.PropertyEquality;
 
+    /// <summary>
+    /// If true, doesn't produce a mismatch, or error, when comparing nulls. Only null is equal to null.
+    /// Defaults to true.
+    /// </summary>
+    public bool AllowNullComparison { get; set; } = true;
+
+    /// <summary>
+    /// If true, allows nulls in the arguments to be compared. If false, passing nulls will result in an error.
+    /// Defaults to true.
+    /// </summary>
+    public bool AllowNullsInArguments { get; set; } = true;
+
+    /// <summary>
+    /// Configuration for comparing double values. 
+    /// (Primitive double type, or any double type which is a property of a complex type)
+    /// </summary>
     public DoubleComparisonConfiguration DoubleConfiguration { get; set; }
         = new DoubleComparisonConfiguration();
 }
--- src/FluentCompare/Configuration/Models/Models.cs
+++ src/FluentCompare/Configuration/Models/Models.cs
@@ -1,5 +1,15 @@
+/// <summary>
+/// Defines how complex types (types that contain other complex or primitive types) are compared.
+/// </summary>
 public enum ComplexTypesComparisonMode
 {
+    /// <summary>
+    /// Compares complex types by their reference. 
+    /// Two references are equal if they point to the same object in memory.
+    /// </summary>
     ReferenceEquality,
+    /// <summary>
+    /// Compares complex types by their public properties.
+    /// </summary>
     PropertyEquality
 }
