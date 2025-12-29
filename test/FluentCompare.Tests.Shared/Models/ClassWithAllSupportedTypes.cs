7a65e09719fda787c368d4ace65c0885191e8d15
Grzegorz Pasterny <grzegorz.pasterny95@gmail.com>
2025-12-29 15:23:09 +0100

    Add unit tests for bool and byte

--- test/FluentCompare.Tests.Shared/Models/ClassWithAllSupportedTypes.cs
+++ test/FluentCompare.Tests.Shared/Models/ClassWithAllSupportedTypes.cs
@@ -1,14 +1,23 @@
 namespace FluentCompare.Tests.Shared.Models;
+
 public class ClassWithAllSupportedTypes
 {
     public int Int { get; set; }
-    public int[] IntArray { get; set; }
-    public string String { get; set; }
-    public string[] StringArray { get; set; }
+    public int[]? IntArray { get; set; }
+    public byte Byte { get; set; }
+    public byte[]? ByteArray { get; set; }
+    public bool Bool { get; set; }
+    public bool[]? BoolArray { get; set; }
+    public string? String { get; set; }
+    public string[]? StringArray { get; set; }
     public double Double { get; set; }
-    public double[] DoubleArray { get; set; }
-    public object Object { get; set; }
-    public object[] ObjectArray { get; set; }
-    public ClassWithAllSupportedTypes NestedClass { get; set; }
-    public ClassWithAllSupportedTypes[] NestedClassArray { get; set; }
+    public double[]? DoubleArray { get; set; }
+    public float Float { get; set; }
+    public float[]? FloatArray { get; set; }
+    public decimal Decimal { get; set; }
+    public decimal[]? DecimalArray { get; set; }
+    public object? Object { get; set; }
+    public object[]? ObjectArray { get; set; }
+    public ClassWithAllSupportedTypes? NestedClass { get; set; }
+    public ClassWithAllSupportedTypes[]? NestedClassArray { get; set; }
 }
--- test/FluentCompare.UnitTests/TestDataGenerator.cs
+++ test/FluentCompare.UnitTests/TestDataGenerator.cs
@@ -6,18 +6,26 @@ namespace FluentCompare.UnitTests;
 
 public static class TestDataGenerator
 {
-    public static ClassWithAllSupportedTypes CreateClassWithAllSupportedTypes(int depth = 1)
+    public static ClassWithAllSupportedTypes? CreateClassWithAllSupportedTypes(int depth = 1)
     {
         if (depth > 2) // prevent infinite recursion
             return null;
 
         var faker = new Faker<ClassWithAllSupportedTypes>()
+            .RuleFor(x => x.Bool, f => f.Random.Bool())
+            .RuleFor(x => x.BoolArray, f => f.Make(3, () => f.Random.Bool()).ToArray())
+            .RuleFor(x => x.Byte, f => f.Random.Byte(0, 100))
+            .RuleFor(x => x.ByteArray, f => f.Make(3, () => f.Random.Byte(0, 100)).ToArray())
             .RuleFor(x => x.Int, f => f.Random.Int(0, 100))
             .RuleFor(x => x.IntArray, f => f.Make(3, () => f.Random.Int(0, 100)).ToArray())
             .RuleFor(x => x.String, f => f.Lorem.Word())
             .RuleFor(x => x.StringArray, f => f.Make(3, () => f.Lorem.Word()).ToArray())
             .RuleFor(x => x.Double, f => f.Random.Double(0, 100))
             .RuleFor(x => x.DoubleArray, f => f.Make(3, () => f.Random.Double(0, 100)).ToArray())
+            .RuleFor(x => x.Float, f => f.Random.Float(0, 100))
+            .RuleFor(x => x.FloatArray, f => f.Make(3, () => f.Random.Float(0, 100)).ToArray())
+            .RuleFor(x => x.Decimal, f => f.Random.Decimal(0, 100))
+            .RuleFor(x => x.DecimalArray, f => f.Make(3, () => f.Random.Decimal(0, 100)).ToArray())
             .RuleFor(x => x.Object, f => f.Random.Word())
             .RuleFor(x => x.ObjectArray, f => f.Make(3, () => (object)f.Random.Word()).ToArray())
             .RuleFor(x => x.NestedClass, _ => CreateClassWithAllSupportedTypes(depth + 1))
